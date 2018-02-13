700f4f17d9438f6b5f96280121106df4d01edb16
Emmanuel Castro Soto <ecastro.invid@hotmail.com>
2018-02-13 09:29:17 -0400

    First Commit

---
+++ src/app/components/itinerary/itinerary.component.ts
@@ -0,0 +1,104 @@
+// import { BaseFormComponent } from "../shared/index";
+// import { TranslateService } from "ng2-translate";
+// import { ToastyService } from "ng2-toasty";
+// import { CookieService } from "angular2-cookie/core";
+import { Component, OnInit } from '@angular/core';
+import 'fullcalendar';
+import * as $ from 'jquery'
+
+// declare var $: any;
+
+@Component({
+	moduleId: module.id,
+	selector: 'ss-itinerary',
+	templateUrl: './itinerary.component.pug',
+	styleUrls: ['itinerary.component.css']
+})
+
+export class ItineraryComponent implements OnInit /*extends BaseFormComponent*/ {
+	public language: string;
+	containerEl: any;
+
+	options: Object = {
+		header: {
+			left: 'prev,next today',
+			center: 'title',
+			right: 'month,agendaWeek,agendaDay,listWeek'
+		},
+		defaultDate: new Date(),
+		navLinks: true, // can click day/week names to navigate views
+		editable: true,
+		eventLimit: true, // allow "more" link when too many events
+		events: [
+			{
+				title: 'All Day Event',
+				start: '2018-02-01',
+			},
+			{
+				title: 'Long Event',
+				start: '2018-02-07',
+				end: '2018-02-10'
+			},
+			{
+				id: 999,
+				title: 'Repeating Event',
+				start: '2018-02-09T16:00:00'
+			},
+			{
+				id: 999,
+				title: 'Repeating Event',
+				start: '2018-02-16T16:00:00'
+			},
+			{
+				title: 'Conference',
+				start: '2018-02-11',
+				end: '2018-02-13'
+			},
+			{
+				title: 'Meeting',
+				start: '2018-02-12T10:30:00',
+				end: '2018-02-12T12:30:00'
+			},
+			{
+				title: 'Lunch',
+				start: '2018-02-12T12:00:00'
+			},
+			{
+				title: 'Meeting',
+				start: '2018-02-12T14:30:00'
+			},
+			{
+				title: 'Happy Hour',
+				start: '2018-02-12T17:30:00'
+			},
+			{
+				title: 'Dinner',
+				start: '2018-02-12T20:00:00'
+			},
+			{
+				title: 'Birthday Party',
+				start: '2018-02-13T07:00:00'
+			},
+			{
+				title: 'Click for Google',
+				url: 'http://google.com/',
+				start: '2018-02-28'
+			}
+		]
+	}
+	constructor(
+		// private translate: TranslateService,
+		// public toastyService: ToastyService,
+		// private _cookieService: CookieService
+	) {
+		// super();
+		// this.translate.use(this._cookieService.get("currentLanguage"));
+		// this.language = this._cookieService.get("currentLanguage");
+	}
+
+	ngOnInit() {
+		this.containerEl = $('#calendar');
+		this.containerEl.fullCalendar(this.options);
+		console.log(this.containerEl);
+	}
+}
